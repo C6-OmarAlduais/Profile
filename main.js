145d5f97db943f3ac3b639567646eca016f8b04a
OmarAlduais <omar.a.alduais@gmail.com>
2022-10-20 17:46:26 +0300

    project

--- main.js
+++ main.js
@@ -2,29 +2,38 @@ const body = $("body");
 const about = $("#about");
 const contact = $("#contact");
 const service = $("#service");
+const project = $("#project");
 const home = $("#home");
 const main = $(".main");
 const main1 = $(".main1");
 const main2 = $(".main2");
-main1.hide()
-main2.hide()
+const main3 = $(".main3");
+main1.hide();
+main2.hide();
+main3.hide();
 
-about.on
-  ("click",
-  () => {
-    main.hide();
-    main1.show()
-  main2.hide()
-
-  });
+about.on("click", () => {
+  main.hide();
+  main1.show();
+  main2.hide();
+  main3.hide();
+});
 home.on("click", () => {
   main.show();
-  main1.hide()
-  main2.hide()
-
+  main1.hide();
+  main2.hide();
+  main3.hide();
 });
-service.on('click', ()=>{
-    main.hide()
-    main1.hide()
-    main2.show()
-})
+service.on("click", () => {
+  main.hide();
+  main1.hide();
+  main2.show();
+  main3.hide();
+});
+
+project.on("click", () => {
+  main.hide();
+  main1.hide();
+  main2.hide();
+  main3.show();
+});
