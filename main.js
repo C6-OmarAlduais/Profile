a524e97344363e8dc99b74d462ee77abd8623ad2
OmarAlduais <omar.a.alduais@gmail.com>
2022-10-20 07:30:36 +0300

    about

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const contact = $("#contact");
 const service = $("#service");
 const home = $("#home");
 const main = $(".main");
+const main1 = $(".main1");
+main1.hide()
 
 about.on
   ("click",
   () => {
     main.hide();
+    main1.show()
   });
 home.on("click", () => {
   main.show();
+  main1.hide()
 });
