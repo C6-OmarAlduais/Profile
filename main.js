8016360725142612a0cabc294377eacf7ba671b8
OmarAlduais <omar.a.alduais@gmail.com>
2022-10-20 11:48:03 +0300

    services

--- main.js
+++ main.js
@@ -5,15 +5,26 @@ const service = $("#service");
 const home = $("#home");
 const main = $(".main");
 const main1 = $(".main1");
+const main2 = $(".main2");
 main1.hide()
+main2.hide()
 
 about.on
   ("click",
   () => {
     main.hide();
     main1.show()
+  main2.hide()
+
   });
 home.on("click", () => {
   main.show();
   main1.hide()
+  main2.hide()
+
 });
+service.on('click', ()=>{
+    main.hide()
+    main1.hide()
+    main2.show()
+})
