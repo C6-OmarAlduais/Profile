108c30070556a17e51bf514c2689faaf844ba56b
OmarAlduais <omar.a.alduais@gmail.com>
2022-10-20 18:30:21 +0300

    update

--- main.js
+++ main.js
@@ -8,27 +8,32 @@ const main = $(".main");
 const main1 = $(".main1");
 const main2 = $(".main2");
 const main3 = $(".main3");
+const main4 = $(".main4");
 main1.hide();
 main2.hide();
 main3.hide();
+main4.hide();
 
 about.on("click", () => {
   main.hide();
   main1.show();
   main2.hide();
   main3.hide();
+  main4.hide();
 });
 home.on("click", () => {
   main.show();
   main1.hide();
   main2.hide();
   main3.hide();
+  main4.hide();
 });
 service.on("click", () => {
   main.hide();
   main1.hide();
   main2.show();
   main3.hide();
+  main4.hide();
 });
 
 project.on("click", () => {
@@ -36,4 +41,13 @@ project.on("click", () => {
   main1.hide();
   main2.hide();
   main3.show();
+  main4.hide();
+});
+
+contact.on("click", () => {
+  main.hide();
+  main1.hide();
+  main2.hide();
+  main3.hide();
+  main4.show();
 });
